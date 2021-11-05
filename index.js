ab0dbfbd72de092a6378e2d7f19b8dc90bbeac13
Paul Bredenberg <paulbredenberg@gmail.com>
2021-11-05 09:24:50 -0400

    fix: switch from slow map to string comparison

--- index.js
+++ index.js
@@ -22,13 +22,7 @@ module.exports = () => {
                declarationRawLines = decl
                   .source
                   .input
-                  .css
-                  .split('\n') // Splits on newlines to create a list of all the lines in the declaration.
-                  .map((line) => {
-                     // Strip out any spaces from the raw incoming statement.
-                     // We can use this for comparison when we build the fallback rule.
-                     return line.replace(/\s+/g, '');
-                  });
+                  .css;
 
                getCSSVarFallbackValue = (value) => {
                   // Capture everything within var() https://regex101.com/r/7UYMv8/1
@@ -51,9 +45,14 @@ module.exports = () => {
                if (cssVarFallbackValue) {
                   const cssFallbackRule = { prop: cssPropertyValue, value: cssVarFallbackValue };
 
+                  let isRuleExisting;
+
+                  isRuleExisting = declarationRawLines
+                     .match(`(${cssFallbackRule.prop}:|${cssFallbackRule.prop}: )${cssFallbackRule.value};`);
+
                   // If a fallback value is already in the statement,
                   // skip insertion of the fallback.
-                  if (declarationRawLines.indexOf(`${cssFallbackRule.prop}:${cssFallbackRule.value};`) > -1) {
+                  if (isRuleExisting) {
                      return;
                   }
 
